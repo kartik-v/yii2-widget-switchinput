3e506542b6f0c71d11f48559a5385d9c434ac0f3
Kartik Visweswaran <kartikv2@gmail.com>
2014-11-09 01:36:31 +0530

    Initial release

---
+++ SwitchInputAsset.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * @copyright Copyright &copy; Kartik Visweswaran, Krajee.com, 2014
+ * @package yii2-widgets
+ * @subpackage yii2-widget-timepicker
+ * @version 1.0.0
+ */
+
+namespace kartik\switch;
+
+/**
+ * Asset bundle for Switch Widget
+ *
+ * @author Kartik Visweswaran <kartikv2@gmail.com>
+ * @since 1.0
+ */
+class SwitchInputAsset extends \kartik\base\AssetBundle
+{
+
+    public function init()
+    {
+        $this->setSourcePath(__DIR__ . '/assets');
+        $this->setupAssets('css', ['css/bootstrap-switch']);
+        $this->setupAssets('js', ['js/bootstrap-switch']);
+        parent::init();
+    }
+}
