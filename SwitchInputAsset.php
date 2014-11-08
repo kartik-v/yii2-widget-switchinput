8e06c83bd3f9879e6fe96d2fac32de77f1bc0f2b
Kartik Visweswaran <kartikv2@gmail.com>
2014-11-09 01:55:50 +0530

    Initial release

--- SwitchInputAsset.php
+++ SwitchInputAsset.php
@@ -17,7 +17,6 @@ namespace kartik\switchinput;
  */
 class SwitchInputAsset extends \kartik\base\AssetBundle
 {
-
     public function init()
     {
         $this->setSourcePath(__DIR__ . '/assets');
